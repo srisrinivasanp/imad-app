ec4bdc7f6ba2c4ad711dc961df5510532eaa103b
srisrinivasanp <36366783+srisrinivasanp@users.noreply.github.com>
2018-02-13 19:58:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ app.get('/', function (req, res) {
 app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.sendFile('article one will be served here');
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile('article two will be served here');
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile('article three will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

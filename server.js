d06ac34f382ec1a14b149327561843da0d9906c6
srisrinivasanp <36366783+srisrinivasanp@users.noreply.github.com>
2018-02-14 17:46:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,32 @@ function createtemplate(data){
     title=data.title;
     heading=data.heading;
     context=data.context;
+    var htmltemplate=`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name= "viewport" content=" width=device-width initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h>
+            ${heading}
+        </h>
+        <div>
+           ${context}
+        </div>
+        </div>
+    </body>
+</html>`;
+return htmltemplate;
+
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -31,7 +57,7 @@ app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
